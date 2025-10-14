b30f5651e05834c7d46b54e3685853c664a78434
Yoda-Flash <kaitlyn.zee@gmail.com>
2025-10-14 09:48:17 -0700

    Wrote Motor Subsystem code

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.commands.ArcadeMotor;
+import frc.robot.constants.IOConstants;
+import frc.robot.subsystems.Motor;
+
+public class RobotContainer {
+  private Joystick m_joystick = new Joystick(IOConstants.kJoystickPort);
+
+  private Motor m_motor = new Motor();
+  private ArcadeMotor m_arcadeMotor = new ArcadeMotor(m_motor, m_joystick);
+
+  public RobotContainer() {
+    m_motor.setDefaultCommand(m_arcadeMotor);
+    configureBindings();
+  }
+
+  private void configureBindings() {}
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
+}
---
+++ src/main/java/frc/robot/constants/IOConstants.java
@@ -0,0 +1,6 @@
+package frc.robot.constants;
+
+public final class IOConstants {
+    public static final int kJoystickPort = 0;
+    public static final int kMotorControlAxis = 0;
+}
